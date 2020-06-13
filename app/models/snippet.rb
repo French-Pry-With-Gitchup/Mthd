1b61f11419a570a249bbc8b97685f9ce8d8d4c4a
JonTDean <JonathanTomDean@Gmail.com>
2020-06-12 20:55:21 -0400

    Finalized Relationships

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,5 @@
 class Snippet < ApplicationRecord
+    belongs_to :user
+    has_many :topics
+    has_many :categories, through: :topics
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,4 @@
 class Topic < ApplicationRecord
+    belongs_to :category
+    belongs_to :snippet
 end
