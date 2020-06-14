f751138cbecd2717ffc134c042268320b8aef247
“dinadeljanin” <ddeljanin5@icloud.com>
2020-06-13 23:30:56 -0400

    began work on snippets // markdown functionality has been added

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -3,4 +3,16 @@ class SnippetsController < ApplicationController
     # flash error here
     @snippets = Snippet.all
   end
+
+  def show
+    @snippet = Snippet.find(params[:id])
+  end
+
+  def new
+    @snippet = Snippet.new
+  end
+
+  def create
+    @snippet = 
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,12 @@ class User < ApplicationRecord
     validates_presence_of :name, :password          # Forces Presence of string values in form fields
     validates_uniqueness_of :name #, on: :create     # Forces Uniqueness of User.name in [User.create | New.HTML.erb]
 
-    # Validates UserName to not use whitespaces or special characters                     
+    # Validates UserName to not use whitespaces or special characters
     validates_with NameValidator
     # validates_with PasswordValidator
 
 
-    validates :password, format: { with: //, 
+    validates :password, format: { with: //,
         message: "Case Sensitive, Requirements: Numbers,letters, the following Special Characters are allowed: !,@,#,$,%,^,&,*,_,- ,=,+,/,\ "}
 
 end
-
-
