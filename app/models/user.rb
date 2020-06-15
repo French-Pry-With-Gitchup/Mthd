de98e2ae96f970eb6404268e42a5d91a6a8085ed
“dinadeljanin” <ddeljanin5@icloud.com>
2020-06-15 00:06:19 -0400

    create an account view fully styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
 class User < ApplicationRecord
-    # include ActiveModel::Validations
+  include ActiveModel::Validations
 
-    has_many :categories
-    has_many :snippets
+  has_many :categories
+  has_many :snippets
 
-    # Validation Checks For User Auth
-    validates_presence_of :name, :password          # Forces Presence of string values in form fields
-    validates_uniqueness_of :name #, on: :create     # Forces Uniqueness of User.name in [User.create | New.HTML.erb]
+  # Validation Checks For User Auth
+  # Forces Presence of string values in form fields
+  validates_presence_of :name, :password
+  validates_uniqueness_of :name #, on: :create     # Forces Uniqueness of User.name in [User.create | New.HTML.erb]
 
-    # Validates UserName to not use whitespaces or special characters
-    validates_with NameValidator
-    # validates_with PasswordValidator
+  # Validates UserName to not use whitespaces or special characters
+  validates_with NameValidator
+  # validates_with PasswordValidator
 
+  validates :password, format: { with: //,
+      message: "Case Sensitive, Requirements: Numbers,letters, the following Special Characters are allowed: !,@,#,$,%,^,&,*,_,- ,=,+,/,\ "}
 
-    validates :password, format: { with: //,
-        message: "Case Sensitive, Requirements: Numbers,letters, the following Special Characters are allowed: !,@,#,$,%,^,&,*,_,- ,=,+,/,\ "}
 
 end
