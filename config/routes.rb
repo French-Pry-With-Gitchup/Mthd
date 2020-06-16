8f81ff03167b3b8c7b688ff99c44c54f57081941
JonTDean <JonathanTomDean@Gmail.com>
2020-06-16 16:27:11 -0400

    Patched up logout issues

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   # match 'sign_up', to: 'users#new', :via => [:get]                      # Alternative to the above
 
   # Handle User Login Features
-  get 'user/login', to: 'users#login_form', as: :login       # Handles Page Data and User Input
-  post 'user/login', to: 'users#handle_login'               # Configures data taken from User
+  get 'user/login', to: 'users#login_form', as: :login        # Handles Page Data and User Input
+  post 'user/login', to: 'users#handle_login'                 # Configures data taken from User
+  post 'user/logout', to: 'users#logout'
 
   # Handle Edit and Delete Functions
   patch 'user/:id', to: 'users#edit'
