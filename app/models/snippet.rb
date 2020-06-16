3a6cc36ef74b1e77825ac13ab4965bbe160d74a0
JonTDean <JonathanTomDean@Gmail.com>
2020-06-16 18:40:51 -0400

    Fixed Category/Snippet Displays

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,5 +2,7 @@ class Snippet < ApplicationRecord
     belongs_to :user
     has_many :topics
     has_many :categories, through: :topics
+
+    validates_presence_of :title, :code, on: :create
 end
 
